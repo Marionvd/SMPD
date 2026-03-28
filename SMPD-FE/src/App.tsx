9634b0e566c5ec46a12d772753311a32a6dcdb02
vadiimXD <vadim123bg@gmail.com>
2026-03-28 16:16:10 +0200

    Update App.tsx

--- SMPD-FE/src/App.tsx
+++ SMPD-FE/src/App.tsx
@@ -1,7 +1,21 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Features from './components/Features/Features';
+import Product from './components/Product/Product';
+import Benefits from './components/Benefits/Benefits';
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <Header />
+      <Home />
+      <Features />
+      <Product />
+      <Benefits />
+      <Contact />
+      <Footer />
     </div>
   );
 }
